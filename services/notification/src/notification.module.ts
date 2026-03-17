f204e89f8e17a40a252dc6cd22fa27ffd3b0a0bf
Adrien <chuadrien2003@gmail.com>
2026-03-17 02:46:28 +0100

    build: notification service

---
+++ services/notification/src/notification.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { NotificationController } from './presentation/notification.controller';
+import { NotificationService } from './application/notification.service';
+
+@Module({
+  controllers: [NotificationController],
+  providers: [NotificationService]
+})
+export class NotificationModule {}
