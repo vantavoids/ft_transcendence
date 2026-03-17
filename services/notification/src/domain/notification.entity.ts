adff85459b16de4a504a7a64f8d02e1d05903fb9
Adrien <chuadrien2003@gmail.com>
2026-03-17 02:46:28 +0100

    build: notification service

---
+++ services/notification/src/domain/notification.entity.ts
@@ -0,0 +1,39 @@
+export enum NotificationType {
+    MENTION = 'mention',
+    DM = 'dm',
+    FRIEND_REQUEST = 'friend_request',
+    GUILD_INVITE = 'guild_invite',
+}
+
+export class MentionPayload {
+    channel_id: string;
+    guild_id: string;
+    author_id: string;
+    preview: string;
+}
+
+export class DmPayload {
+    sender_id: string;
+    preview: string;
+}
+
+export class FriendRequestPayload {
+    from_user_id: string;
+}
+
+export class GuildInvitePayload {
+    guild_id: string;
+    guild_name: string;
+    from_user_id: string;
+}
+
+export type NotificationPayload = MentionPayload | DmPayload | FriendRequestPayload | GuildInvitePayload;
+
+export class Notification {
+    id: string;
+    userId: string;
+    type: NotificationType;
+    payload: NotificationPayload;
+    read: boolean;
+    created_at: string;
+}
