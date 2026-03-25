7b8f8004baddd42259f5cf1c0b231d0a14531968
Adrien <chuadrien2003@gmail.com>
2026-03-25 12:53:26 +0100

    feat(notification): add skeleton for Rest API

--- services/notification/src/presentation/notification.controller.ts
+++ services/notification/src/presentation/notification.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Param, Query, Request } from '@nestjs/common';
+import { NotificationService } from "src/application/notification.service";
+import { NotificationEntity } from "src/domain/notification.entity";
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+	constructor(private readonly notificationService: NotificationService) {}
+
+	@Get()
+		getNotif(@Request() req, @Query('read') read?: boolean, @Query('limit') limit?: number) {
+			const uid = req.user.sub;
+			return this.notificationService.get(uid);
+		}
+
+	@Patch(':id/read')
+		read(@Param('id') uid: string) {
+			return this.notificationService.read(uid);
+		}
+
+	@Patch('read-all')
+		readAll(@Request() req) {
+			const uid = req.user.sub;
+			return this.notificationService.readAll(uid);
+		}
+}
