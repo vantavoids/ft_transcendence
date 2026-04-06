fa2daaff0d474a52d1dd062b38c7df7bc92687ae
um4ss <sliziard@icloud.com>
2026-04-06 15:10:59 +0200

    feat(auth): set PostgreSQL in persistence layer and add appsettings.json

--- services/auth/src/Auth.Persistence/DependencyInjection.cs
+++ services/auth/src/Auth.Persistence/DependencyInjection.cs
@@ -1,8 +1,19 @@
+using Auth.Persistence.Db;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Auth.Persistence;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services) => services;
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AuthDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("AuthDb")));
+
+        return services;
+    }
 }
