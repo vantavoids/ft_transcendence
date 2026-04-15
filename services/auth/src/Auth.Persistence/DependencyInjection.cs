15e015341bc110407cad9c45cecd6130cc49e0a7
um4ss <sliziard@icloud.com>
2026-04-15 15:47:55 +0200

    feat(auth): replace ConnectionStrings by DbOptions
    for db config

--- services/auth/src/Auth.Persistence/DependencyInjection.cs
+++ services/auth/src/Auth.Persistence/DependencyInjection.cs
@@ -1,18 +1,23 @@
 using Auth.Persistence.Db;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Auth.Persistence;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistence(
-        this IServiceCollection services,
-        IConfiguration configuration)
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
-        services.AddDbContext<AuthDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("AuthDb")));
+        services.AddOptions<DbOptions>()
+            .BindConfiguration(DbOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddDbContext<AuthDbContext>((ctx, config) => {
+            var options = ctx.GetRequiredService<IOptions<DbOptions>>().Value;
+            config.UseNpgsql(options.ToConnectionString());
+        });
 
         return services;
     }
