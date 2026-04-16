e37f86387e52321eac8d946fe47aecfb173d946f
um4ss <sliziard@icloud.com>
2026-04-16 19:44:44 +0200

    feat(auth/pres): implement register endpoint
    Add records to represent contract responses

---
+++ services/auth/src/Auth.Presentation/Endpoints/RegisterEndpoint.cs
@@ -0,0 +1,62 @@
+using Carter;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Auth.Application.Abstractions.Messaging;
+using Auth.Application.Features.Register;
+using Auth.Domain.Results;
+using Auth.Infrastructure.Options;
+using Microsoft.Extensions.Options;
+using Auth.Application.Abstractions;
+using Auth.Presentation.Contracts.Responses;
+
+public sealed class RegisterEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("/register", async (
+            RegisterCommand req,
+            ICommandHandler<RegisterCommand, Result<RegisterResult>> handler,
+            IClock clock,
+            IOptions<RefreshTokenOptions> options,
+            HttpResponse resp
+        ) => await Register(req, handler, clock, options.Value, resp));
+    }
+
+
+    private async Task<
+        Results<Created<RegisterResponse>, BadRequest<ErrorResponse>, Conflict<ErrorResponse>>
+    > Register(
+        RegisterCommand req,
+        ICommandHandler<RegisterCommand, Result<RegisterResult>> handler,
+        IClock clock,
+        RefreshTokenOptions options,
+        HttpResponse resp
+    )
+    {
+        var result = await handler.HandleAsync(req);
+
+        return result.Match<
+            Results<Created<RegisterResponse>, BadRequest<ErrorResponse>, Conflict<ErrorResponse>>
+        >(
+            r => {
+                resp.Cookies.Append(
+                    options.CookieName,
+                    r.RefreshToken,
+                    new CookieOptions
+                    {
+                        HttpOnly = options.HttpOnly,
+                        Secure = options.Secure,
+                        SameSite = SameSiteMode.Strict,
+                        Expires = clock.UtcNow.AddDays(options.TtlDays)
+                    }
+                );
+                return TypedResults.Created($"/users/{r.UserId}", new RegisterResponse(r.UserId, r.AccessToken));
+            },
+            failure => {
+                var errResp = new ErrorResponse(failure.Message);
+                if (failure.Code == AuthFailures.EmailAlreadyRegistered.Code)
+                    return TypedResults.Conflict(errResp);
+                return TypedResults.BadRequest(errResp);
+            }
+        );
+    }
+}
