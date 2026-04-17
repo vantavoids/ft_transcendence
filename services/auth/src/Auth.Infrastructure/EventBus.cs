f61299f83859f5b2596a77bccc090f98f37f4d05
um4ss <sliziard@icloud.com>
2026-04-17 13:13:27 +0200

    feat(auth/infra): refine EventBus with IEvent and add SystemClock class

--- services/auth/src/Auth.Infrastructure/EventBus.cs
+++ services/auth/src/Auth.Infrastructure/EventBus.cs
@@ -1,11 +1,12 @@
-using Auth.Application.Abstractions;
+using Auth.Application.Abstractions.Events;
 using MassTransit;
 
-namespace Auth.Infrastructure.Messaging;
+namespace Auth.Infrastructure;
 
 internal sealed class EventBus(IPublishEndpoint publishEndpoint) : IEventBus
 {
-    public Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class
+    public Task PublishAsync<T>(T message, CancellationToken cancellationToken = default)
+        where T : class, IEvent
     {
         return publishEndpoint.Publish(message, cancellationToken);
     }
