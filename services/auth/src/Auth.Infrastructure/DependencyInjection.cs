e1bb7ed4e03e82728bac4e40150e75f9cf90d777
um4ss <sliziard@icloud.com>
2026-04-17 15:30:00 +0200

    feat(auth/infra): add DI for services and options
    All options that implement the IOptions interface are automatically added to the DI

--- services/auth/src/Auth.Infrastructure/DependencyInjection.cs
+++ services/auth/src/Auth.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,50 @@
+using System.Reflection;
+using Auth.Application.Abstractions;
+using Auth.Application.Abstractions.Events;
+using Auth.Application.Abstractions.Security;
+using Auth.Infrastructure.Options;
+using Auth.Infrastructure.Security;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Auth.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services) => services;
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        var optionsTypes = typeof(DependencyInjection).Assembly.GetTypes()
+            .Where(t => t is { IsAbstract: false, IsInterface: false } &&
+                        typeof(IOptions).IsAssignableFrom(t));
+
+        foreach (var optionType in optionsTypes)
+        {
+            var configureOptionsMethod = typeof(DependencyInjection)
+                .GetMethod("ConfigureOptions", BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(optionType);
+
+            configureOptionsMethod.Invoke(null, [services]);
+        }
+
+        services.AddSingleton<IEventBus, EventBus>();
+        services.AddSingleton<IClock, SystemClock>();
+        services.AddSingleton<IIdGenerator, SnowflakeIdGenerator>();
+        services.AddSingleton<ISecretHasher, BcryptHasher>();
+        services.AddSingleton<ITokenGenerator, TokenGenerator>();
+
+        // services.AddKeyedTransient<IOAuthProviderClient, GithubOAuthProviderClient>(OAuthProvider.Github);
+        // services.AddKeyedTransient<IOAuthProviderClient, GoogleOAuthProviderClient>(OAuthProvider.Google);
+        // services.AddKeyedTransient<IOAuthProviderClient, FortyTwoOAuthProviderClient>(OAuthProvider.FortyTwo);
+
+        return services;
+    }
+
+    private static OptionsBuilder<T> ConfigureOptions<T>(IServiceCollection services)
+        where T : class, IOptions
+    {
+        return services.AddOptions<T>()
+            .BindConfiguration(T.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
 }
