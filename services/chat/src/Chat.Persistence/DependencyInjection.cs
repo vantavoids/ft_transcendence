f5e51024168ae4fd7da16cabf7c0e9190a347a4a
Yanis Andry <yandry@student.42.fr>
2026-05-07 14:51:20 +0200

    build(chat): connect to scylladb instance

--- services/chat/src/Chat.Persistence/DependencyInjection.cs
+++ services/chat/src/Chat.Persistence/DependencyInjection.cs
@@ -1,4 +1,7 @@
+using Cassandra;
+using Chat.Persistence.Options;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Chat.Persistence;
 
@@ -6,6 +9,22 @@ public static class DependencyInjection
 {
 	public static IServiceCollection AddPersistence(this IServiceCollection services)
 	{
+		services.AddOptions<ScyllaDbOptions>()
+			.BindConfiguration("ScyllaDb")
+			.ValidateDataAnnotations()
+			.ValidateOnStart();
+
+		services.AddSingleton<ISession>(sp =>
+				{
+					var opts = sp.GetRequiredService<IOptions<ScyllaDbOptions>>().Value;
+					var builder = Cluster.Builder()
+					.AddContactPoints(opts.ContactPoints.Split(','))
+					.WithPort(opts.Port);
+
+					if (!string.IsNullOrEmpty(opts.Username))
+						builder.WithCredentials(opts.Username, opts.Password);
+					return builder.Build().Connect(opts.Keyspace);
+				});
 		return services;
 	}
 }
