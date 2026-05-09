dfba42f4aee3041a925e19c8e6dc1914223fdf49
Yanis Andry <yandry@student.42.fr>
2026-05-09 16:55:49 +0200

    feat(chat): expose signaling hub

--- services/chat/src/Chat.Presentation/Program.cs
+++ services/chat/src/Chat.Presentation/Program.cs
@@ -28,5 +28,6 @@ if (app.Environment.IsDevelopment())
 app.MapHealthChecks("/healthz");
 app.MapCarter();
 app.MapHub<ChatHub>("/hubs/chat");
+app.MapHub<SignalingHub>("/hubs/signaling");
 
 app.Run();
