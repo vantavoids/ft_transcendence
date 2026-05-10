e44c0ed4fca1aab9076564bddebe13a9c7afd286
um4ss <sliziard@icloud.com>
2026-05-10 16:38:45 +0200

    refactor(auth): add ChangeOAuthProviderToInt sql migration

--- services/auth/src/Auth.Persistence/Migrations/20260510133747_ChangeOAuthProviderToInt.Designer.cs
+++ services/auth/src/Auth.Persistence/Migrations/20260510133747_ChangeOAuthProviderToInt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Auth.Persistence.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Auth.Persistence.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260510133747_ChangeOAuthProviderToInt")]
+    partial class ChangeOAuthProviderToInt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
