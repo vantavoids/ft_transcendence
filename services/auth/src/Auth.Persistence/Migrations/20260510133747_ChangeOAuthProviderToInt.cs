de44af3529adaab2d0a7cbbd03a4b004692f0e03
um4ss <sliziard@icloud.com>
2026-05-10 16:38:45 +0200

    refactor(auth): add ChangeOAuthProviderToInt sql migration

---
+++ services/auth/src/Auth.Persistence/Migrations/20260510133747_ChangeOAuthProviderToInt.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOAuthProviderToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE auth_users ALTER COLUMN oauth_provider TYPE integer USING oauth_provider::integer;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE auth_users ALTER COLUMN oauth_provider TYPE character varying(32) USING oauth_provider::varchar;"
+            );
+        }
+    }
+}
