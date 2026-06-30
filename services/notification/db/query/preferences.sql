111c954f2633f50538b70e41b201241410bc527d
Adrien <chuadrien2003@gmail.com>
2026-06-30 22:26:57 +0200

    feat(notification): add remove preference

--- services/notification/db/query/preferences.sql
+++ services/notification/db/query/preferences.sql
@@ -1,3 +1,7 @@
 -- name: ListNotificationPreferences :many
 SELECT * FROM notification_preferences
 WHERE user_id = $1;
+
+-- name: RemoveNotificationPreference :execrows
+DELETE FROM notification_preferences
+WHERE user_id = $1 AND scope_type = $2 AND scope_id = $3;
