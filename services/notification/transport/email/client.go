d8b23b2a79532bb93ffc3a10e36403619e11232b
Adrien <chuadrien2003@gmail.com>
2026-07-08 01:54:55 +0200

    feat(notification): add send email

--- services/notification/transport/email/client.go
+++ services/notification/transport/email/client.go
@@ -30,3 +30,25 @@ func newClient() (*mail.Client, error) {
 		mail.WithPassword(pass),
 	)
 }
+
+func Send(name string, to string, subject string, data any) error {
+
+	msg := mail.NewMsg()
+
+	if err := msg.From(os.Getenv("SMTP_FROM")); err != nil {
+		return fmt.Errorf("failed to set FROM address: %w", err)
+	}
+
+	if err := msg.To(to); err != nil {
+		return fmt.Errorf("failed to set TO address: %s", err)
+	}
+
+	msg.Subject(subject)
+
+	client, err := newClient()
+	if err != nil {
+		return fmt.Errorf("failed to create smtp client: %w", err)
+	}
+
+	return client.DialAndSend(msg)
+}
