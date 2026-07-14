00b860152e6bbb9b87e370a3e809c890c805c664
Yanis Andry <yandry@student.42.fr>
2026-07-14 02:47:50 +0200

    test(front): wrap ChannelList test in NotificationPrefsProvider

--- frontend/tests/channel-list.test.tsx
+++ frontend/tests/channel-list.test.tsx
@@ -3,6 +3,7 @@ import { describe, it } from 'node:test';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { ChannelList, type ChannelCategory } from '../src/components/channel-list';
 import { GuildProvider } from '../src/shared/guilds/guild-store';
+import { NotificationPrefsProvider } from '../src/shared/lib/notification-prefs-store';
 
 const categories: ChannelCategory[] = [
   {
@@ -16,22 +17,24 @@ describe('ChannelList', () => {
   it('renders a guild settings entry in chat mode', () => {
     const html = renderToStaticMarkup(
       <GuildProvider>
-        <ChannelList
-          activeChannel="chan-1"
-          categories={categories}
-          unreadCounts={{}}
-          mobilePane="channels"
-          currentUser={null}
-          isMicMuted={false}
-          isDeafened={false}
-          unreadNotifications={0}
-          onToggleDeafen={() => undefined}
-          onToggleMicMute={() => undefined}
-          onOpenNotifications={() => undefined}
-          onOpenSettings={() => undefined}
-          onOpenGuildSettings={() => undefined}
-          onSelectChannel={() => undefined}
-        />
+        <NotificationPrefsProvider>
+          <ChannelList
+            activeChannel="chan-1"
+            categories={categories}
+            unreadCounts={{}}
+            mobilePane="channels"
+            currentUser={null}
+            isMicMuted={false}
+            isDeafened={false}
+            unreadNotifications={0}
+            onToggleDeafen={() => undefined}
+            onToggleMicMute={() => undefined}
+            onOpenNotifications={() => undefined}
+            onOpenSettings={() => undefined}
+            onOpenGuildSettings={() => undefined}
+            onSelectChannel={() => undefined}
+          />
+        </NotificationPrefsProvider>
       </GuildProvider>
     );
 
